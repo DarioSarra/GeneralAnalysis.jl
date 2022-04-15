553afbbe78d27a8ea676126500d7106eb954eb51
Documenter.jl <documenter@juliadocs.github.io>
2022-04-15 17:38:15 +0000

    build based on 210ca04

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeneralAnalysis","category":"page"},{"location":"#GeneralAnalysis","page":"Home","title":"GeneralAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeneralAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeneralAnalysis]","category":"page"}]
+}
